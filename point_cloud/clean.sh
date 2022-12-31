dceb68196325a388360cd64631067220645a1ea5
Alessio240593 <alessiozattoni93@gmail.com>
2022-12-31 10:04:07 +0100

    Aggiornato il progetto point_cloud

--- point_cloud/clean.sh
+++ point_cloud/clean.sh
@@ -2,6 +2,6 @@
 
 clear 
 
-rm -rf bin && rm -rf build
+rm -rf bin && rm -rf build && rm -rf pcd_images
 
 echo "bin, build and calibration_setup directory removed successfully!"
--- point_cloud/install.sh
+++ point_cloud/install.sh
@@ -7,6 +7,12 @@ then
     rm -rf build
 fi
 
+if [ -d "pcd_images" ]
+then
+    rm -rf pcd_images
+fi
+
 mkdir build
+mkdir pcd_images
 cd build
 cmake .. && make
